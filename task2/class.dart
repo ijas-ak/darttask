249de12fecd51d809813f74301fdba010613064a
ijas-ak <ijasakey82@gmail.com>
2025-03-14 14:11:10 +0530

    Updated

--- task2/class.dart
+++ task2/class.dart
@@ -1,25 +1,31 @@
 class Person {
   String? name;
   dynamic age;
-  dynamic password;
+  String? sex;
   Person(
       {required String? name,
       dynamic age = "Not Defined",
-      required dynamic password}) {
+      required String? sex}) {
     this.name = name;
     this.age = age;
-    this.password = password;
+    this.sex = sex;
   }
   void persondetails() {
     print("name = $name");
     print("age = $age");
-    print("Password = $password");
+    print("Gender = $sex");
   }
 }
 
 void main() {
-  Person p1 = Person(password: 'ijasak123', name: "ijas ak");
+  Person p1 = Person(name: "ijas ak", age: 21, sex: 'Male');
   p1.persondetails();
-  Person p2 = Person(password: 'muhiajs', name: "gvghgh");
+  Person p2 = Person(name: "Rahil", age: 22, sex: "Male");
   p2.persondetails();
+  Person p3 = Person(name: "Yaseen", age: 13, sex: "Male");
+  p3.persondetails();
+  Person p4 = Person(name: "Ibrahim ak", age: 44, sex: "Male");
+  p4.persondetails();
+  Person p5 = Person(name: "Jaseela", age: 36, sex: "Female");
+  p5.persondetails();
 }
